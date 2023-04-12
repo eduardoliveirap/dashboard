17002f24323804e6b163af46855cd9fe00ca13eb
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-04-12 10:00:27 -0300

    fizemos a pagina de categorias

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
+//CHECA SE O LIGHTMODE ESTÁ ATIVO
+let lightMode = localStorage.getItem('lightMode');
+if(lightMode === 'true'){
+    modeSwitch.classList.toggle("active");
+    topo.classList.toggle("light");
+}
+
+//AO CLICAR NO SWITCH ALTERNA ENTRE LIGHT E DARK MODE
 modeSwitch.onclick = (e) => {
     e.target.classList.toggle("active");
     topo.classList.toggle("light");
+    if(topo.classList.contains('light')){
+        localStorage.setItem('lightMode', true);
+    }else{
+        localStorage.setItem('lightMode', false);
+    }
+}
+
+let btnAbrir = document.querySelector(".abrir-lateral");
+btnAbrir.onclick = function(){
+    lateral.classList.add("active");
 }
