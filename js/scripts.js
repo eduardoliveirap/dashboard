760eba6300631232c7ee3ec4f2faf8beaa4d958f
ursarah <sarahurcezino@gmail.com>
2023-04-19 08:14:10 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,10 @@ let btnAbrir = document.querySelector(".abrir-lateral");
 btnAbrir.onclick = function(){
     lateral.classList.add("active");
 }
+
+let btnFechar = document.querySelector(".fechar-lateral");
+btnFechar.onclick = function(){
+    lateral.classList.remove("active");
+}
+
+
